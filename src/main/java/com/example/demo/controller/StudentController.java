ae5ac5e08fe2059bd39e9e728bd3ddb47fb0e2a5
shenxinyuan <javashenxinyuan@163.com>
2023-01-19 10:52:09 +0800

    更新fastJson -> fastJson2

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,6 +1,6 @@
 package com.example.demo.controller;
 
-import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson2.JSON;
 import com.example.demo.controller.vo.request.StudentRequest;
 import com.example.demo.entity.dto.StudentDto;
 import com.example.demo.service.IStudentService;
--- src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
@@ -1,6 +1,6 @@
 package com.example.demo.service.impl;
 
-import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson2.JSON;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.example.demo.entity.StudentEntity;
 import com.example.demo.entity.dto.StudentDto;

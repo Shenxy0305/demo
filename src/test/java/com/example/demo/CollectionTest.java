f5b64c31286e4be8256931ebe757ee0071e259be
shenxinyuan <javashenxinyuan@163.com>
2023-02-02 21:33:59 +0800

    collectionTest

--- src/test/java/com/example/demo/CollectionTest.java
+++ src/test/java/com/example/demo/CollectionTest.java
@@ -2,17 +2,24 @@ package com.example.demo;
 
 import lombok.Builder;
 import lombok.Data;
+import org.assertj.core.api.MapAssert;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 public class CollectionTest {
 
 
     @Data
+    @Builder
+    private static class Team {
+        private String name;
+        private List<User> list;
+    }
+
+    @Data
+    @Builder
     private static class User {
         private String account;
         private String password;
@@ -20,26 +27,36 @@ public class CollectionTest {
     }
 
     public static void main(String[] args) {
-        // 健值对是 一个key和一个value
-        // Map结构可以通过key获得对应的value
+        Team team1 = Team.builder().name("team1").list(new ArrayList<>()).build();
+        Team team2 = Team.builder().name("team2").list(new ArrayList<>()).build();
 
-        // 新增一个Map对象 key是Integer类型 value是String类型
-        Map<Integer, String> map = new HashMap<>();
+        for (int i = 0; i < 10; i++) {
+            team1.getList().add(User.builder().account("Team1 account" + i).password("Team1 password" + i).build());
+            team2.getList().add(User.builder().account("Team2 account" + i).password("Team2 password" + i).build());
+        }
 
-        // 将一个健值对放入这个map key是1 value是aaa
-        map.put(1, "aaa");
 
-        // 通过key获取map当中的值 key是1
-        String str = map.get(1);
+        // 有两个Team team1 team2 没个Team里都有十个User
 
-        // TODO: 创建一个User对象 将这个对象的account设为test password设为aaa
+        // TODO: 打印team1 里边 第三个user的account
 
-        // TODO: 创建一个User对象 将这个对象的account设为bbb password设为123
+        // TODO: 打印team2 里边 第十个user的password
 
-        // TODO: 将这两个对象放入map当中 key是user对象的account value是这个user对象
 
-        // TODO: 获取account是bbb的user对象
 
-        // TODO: 获取account是test的user对象的password
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
